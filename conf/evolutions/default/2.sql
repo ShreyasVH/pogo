daa39e0bf7131d53862ef2441fc27634a4c43346
Shreyas Hande <shreyas@everwell.org>
2021-11-12 23:24:55 +0530

    added migrations

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,38 @@
+INSERT INTO `types` (`id`, `name`) VALUES
+("1", "Bug"),
+("2", "Dark"),
+("3", "Dragon"),
+("4", "Electric"),
+("5", "Fairy"),
+("6", "Fighting"),
+("7", "Fire"),
+("8", "Flying"),
+("9", "Ghost"),
+("10", "Grass"),
+("11", "Ground"),
+("12", "Ice"),
+("13", "Normal"),
+("14", "Poison"),
+("15", "Psychic"),
+("16", "Rock"),
+("17", "Steel"),
+("18", "Water");
+
+INSERT INTO `regions` (`id`, `name`) VALUES
+("1", "Kanto"),
+("2", "Johto"),
+("3", "Hoenn"),
+("4", "Sinnoh"),
+("5", "Unova"),
+("6", "Kalos"),
+("7", "Alola"),
+("8", "Galar");
+
+INSERT INTO `items` (`id`, `name`) VALUES
+("1", "Sinnoh Stone"),
+("2", "Unova Stone"),
+("3", "Metal Coat"),
+("4", "Sun Stone"),
+("5", "King's Rock"),
+("6", "Dragon Scale"),
+("7", "Upgrade");
