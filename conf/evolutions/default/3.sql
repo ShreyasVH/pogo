6662f134ae92afcddadf3aeafeb905cf1499bcb1
Shreyas Hande <shreyas@everwell.org>
2021-12-24 23:33:10 +0530

    sql changes

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,14 +2,7 @@ CREATE TABLE `forms` (
     `id`                          int unsigned AUTO_INCREMENT NOT NULL,
     `name`                        varchar(100) NOT NULL,
     `pokemon_number`              int unsigned NOT NULL,
-    `image_url`                   varchar(100),
-    `release_date`                int,
-    `is_alolan`                   boolean,
-    `is_galarian`                 boolean,
-    `is_shadow`                   boolean,
-    `is_shiny`                    boolean,
-    `is_female`                   boolean,
-    `is_costumed`                 boolean,
+    `image_url`                   varchar(200),
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_p_name_number` (`name`, `pokemon_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
