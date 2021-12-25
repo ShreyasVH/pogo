b7193645793ca10c7361d6b49a4bc1e0098e1b27
Shreyas Hande <shreyas@everwell.org>
2021-12-25 16:36:39 +0530

    corrected nigration file

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -3,6 +3,7 @@ CREATE TABLE `forms` (
     `name`                        varchar(100) NOT NULL,
     `pokemon_number`              int unsigned NOT NULL,
     `image_url`                   varchar(200),
+    `release_date`                bigint,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_p_name_number` (`name`, `pokemon_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
